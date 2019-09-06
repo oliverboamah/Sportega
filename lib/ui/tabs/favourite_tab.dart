02b1db766d90e60315eecf1c6118bc6495317bf6
Oliver Boamah <oliverboamah@yahoo.com>
2019-09-05 23:47:01 -1200

    Update favorite tab

--- lib/ui/tabs/favourite_tab.dart
+++ lib/ui/tabs/favourite_tab.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/widgets.dart';
+import 'package:sportega/ui/components/favorite/favorite_news_list.dart';
+import 'package:sportega/ui/components/text_header.dart';
+import 'package:sportega/ui/holders/news.dart';
 
 class FavouriteTab extends StatefulWidget {
   @override
@@ -6,10 +9,35 @@ class FavouriteTab extends StatefulWidget {
 }
 
 class _FavouriteTabState extends State<FavouriteTab> {
+  List<News> newsList = [
+    News(
+        image: 'assets/images/rooney.png',
+        title: 'Why Rooney is the best forward ever at Manchester United',
+        category: 'bbc-sport'),
+    News(
+        image: 'assets/images/drogba.jpg',
+        title:
+            'Why Didier Drogba could be a better chelsea manager than Lampard',
+        category: 'bbc-sport'),
+    News(
+        image: 'assets/images/rooney.png',
+        title: 'Why Rooney is the best forward ever at Manchester United',
+        category: 'bbc-sport'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('This is the Favorite Tab'),
+    return Column(
+      children: <Widget>[
+        TextHeader(
+          title: 'Favourites',
+          subTitle: '${this.newsList.length} articles',
+        ),
+        FavoriteNewsList(
+          newsList: this.newsList,
+          onNewsItemClicked: (position) => print(position),
+        )
+      ],
     );
   }
 }

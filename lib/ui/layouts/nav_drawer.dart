4930d24e6e53f59d2dada84c259e171fee957ec4
Oliver Boamah <oliverboamah@yahoo.com>
2019-09-07 03:15:24 -1200

    Update navigation drawer.

--- lib/ui/layouts/nav_drawer.dart
+++ lib/ui/layouts/nav_drawer.dart
@@ -3,11 +3,38 @@ import 'package:flutter/material.dart';
 import 'package:sportega/values/colors/colors.dart';
 
 class NavDrawer extends StatefulWidget {
+  final String logo = 'assets/images/logo.png';
+  final String title = 'Sportega';
+  final String subTitle = 'sports in your pocket';
+  final List<String> menuTitles = [
+    'News',
+    'Video',
+    'Photo',
+    'Favorite',
+    'About'
+  ];
+  final List<IconData> menuIcons = [
+    CupertinoIcons.news_solid,
+    CupertinoIcons.video_camera_solid,
+    CupertinoIcons.photo_camera_solid,
+    CupertinoIcons.heart_solid,
+    Icons.info
+  ];
+
+  final menuItemColor = Color(PRIMARY_TEXT_COLOR);
+  final selectedMenuItemColor = Color(PRIMARY_COLOR);
+
+  final Function onMenuItemClicked;
+
+  NavDrawer({@required this.onMenuItemClicked});
+
   @override
   State<StatefulWidget> createState() => _NavDrawerState();
 }
 
 class _NavDrawerState extends State<NavDrawer> {
+  String selectedMenuItem = '';
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -17,19 +44,154 @@ class _NavDrawerState extends State<NavDrawer> {
           // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
           children: <Widget>[
-            DrawerHeader(
-              child: Text('Drawer Header'),
+            Container(
               decoration: BoxDecoration(
                 color: Color(PRIMARY_COLOR),
               ),
+              padding: EdgeInsets.only(bottom: 16),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.topLeft,
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.close,
+                        color: Colors.white,
+                      ),
+                      onPressed: () => Navigator.of(context).pop(),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        radius: 60,
+                        backgroundImage: AssetImage(this.widget.logo),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0, bottom: 2),
+                        child: Text(
+                          this.widget.title,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16),
+                        ),
+                      ),
+                      Text(
+                        this.widget.subTitle,
+                        style: TextStyle(
+                            fontSize: 13, color: Colors.white.withOpacity(0.8)),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
             ListTile(
-              title: Text('Item 1'),
-              onTap: () {},
+              title: Text(
+                this.widget.menuTitles[0],
+                style: TextStyle(
+                    color: this.selectedMenuItem == this.widget.menuTitles[0]
+                        ? this.widget.selectedMenuItemColor
+                        : this.widget.menuItemColor),
+              ),
+              leading: IconButton(
+                icon: Icon(
+                  this.widget.menuIcons[0],
+                  color: this.selectedMenuItem == this.widget.menuTitles[0]
+                      ? this.widget.selectedMenuItemColor
+                      : this.widget.menuItemColor,
+                ),
+                onPressed: () => {},
+              ),
+              onTap: () {
+                this.setState(
+                    () => this.selectedMenuItem = this.widget.menuTitles[0]);
+                this.widget.onMenuItemClicked(this.widget.menuTitles[0]);
+                Navigator.of(context).pop();
+              },
             ),
             ListTile(
-              title: Text('Item 2'),
-              onTap: () {},
+              title: Text(this.widget.menuTitles[1],
+                  style: TextStyle(
+                      color: this.selectedMenuItem == this.widget.menuTitles[1]
+                          ? this.widget.selectedMenuItemColor
+                          : this.widget.menuItemColor)),
+              leading: IconButton(
+                icon: Icon(this.widget.menuIcons[1],
+                    color: this.selectedMenuItem == this.widget.menuTitles[1]
+                        ? this.widget.selectedMenuItemColor
+                        : this.widget.menuItemColor),
+                onPressed: () => {},
+              ),
+              onTap: () {
+                this.setState(
+                    () => this.selectedMenuItem = this.widget.menuTitles[1]);
+                this.widget.onMenuItemClicked(this.widget.menuTitles[1]);
+                Navigator.of(context).pop();
+              },
+            ),
+            ListTile(
+              title: Text(this.widget.menuTitles[2],
+                  style: TextStyle(
+                      color: this.selectedMenuItem == this.widget.menuTitles[2]
+                          ? this.widget.selectedMenuItemColor
+                          : this.widget.menuItemColor)),
+              leading: IconButton(
+                icon: Icon(this.widget.menuIcons[2],
+                    color: this.selectedMenuItem == this.widget.menuTitles[2]
+                        ? this.widget.selectedMenuItemColor
+                        : this.widget.menuItemColor),
+                onPressed: () => {},
+              ),
+              onTap: () {
+                this.setState(
+                    () => this.selectedMenuItem = this.widget.menuTitles[2]);
+                this.widget.onMenuItemClicked(this.widget.menuTitles[2]);
+                Navigator.of(context).pop();
+              },
+            ),
+            ListTile(
+              title: Text(this.widget.menuTitles[3],
+                  style: TextStyle(
+                      color: this.selectedMenuItem == this.widget.menuTitles[3]
+                          ? this.widget.selectedMenuItemColor
+                          : this.widget.menuItemColor)),
+              leading: IconButton(
+                icon: Icon(this.widget.menuIcons[3],
+                    color: this.selectedMenuItem == this.widget.menuTitles[3]
+                        ? this.widget.selectedMenuItemColor
+                        : this.widget.menuItemColor),
+                onPressed: () => {},
+              ),
+              onTap: () {
+                this.setState(
+                    () => this.selectedMenuItem = this.widget.menuTitles[3]);
+                this.widget.onMenuItemClicked(this.widget.menuTitles[3]);
+                Navigator.of(context).pop();
+              },
+            ),
+            Divider(),
+            ListTile(
+              title: Text(this.widget.menuTitles[4],
+                  style: TextStyle(
+                      color: this.selectedMenuItem == this.widget.menuTitles[4]
+                          ? this.widget.selectedMenuItemColor
+                          : this.widget.menuItemColor)),
+              leading: IconButton(
+                icon: Icon(this.widget.menuIcons[4],
+                    color: this.selectedMenuItem == this.widget.menuTitles[4]
+                        ? this.widget.selectedMenuItemColor
+                        : this.widget.menuItemColor),
+                onPressed: () => {},
+              ),
+              onTap: () {
+                this.setState(
+                    () => this.selectedMenuItem = this.widget.menuTitles[4]);
+                this.widget.onMenuItemClicked(this.widget.menuTitles[4]);
+                Navigator.of(context).pop();
+              },
             ),
           ],
         ),
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -8,37 +8,64 @@ import 'package:sportega/ui/tabs/news_tab.dart';
 import 'package:sportega/ui/tabs/photo_tab.dart';
 import 'package:sportega/ui/tabs/video_tab.dart';
 
-
 class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _HomePageState();
 }
 
 class _HomePageState extends State {
-
   // seletected tab index
   int selectedTabIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: getHomeAppBar(),
-      drawer: NavDrawer(),
-      bottomNavigationBar: BottomNav(
-        onItemSelected: (index)=> this.setState(()=>this.selectedTabIndex = index)
-      ),
-      backgroundColor: Colors.white,
-      body: this._showTab()
-    );
+        appBar: getHomeAppBar(),
+        drawer: NavDrawer(
+          onMenuItemClicked: (title) => this._onDrawerMenuItemClicked(title),
+        ),
+        bottomNavigationBar: BottomNav(
+            onItemSelected: (index) =>
+                this.setState(() => this.selectedTabIndex = index)),
+        backgroundColor: Colors.white,
+        body: this._showTab());
   }
 
   Widget _showTab() {
-    switch (this.selectedTabIndex){
-      case 0: return NewsTab();
-      case 1: return VideoTab();
-      case 2: return PhotoTab();
-      case 3: return FavouriteTab();
-      default: return NewsTab();
+    switch (this.selectedTabIndex) {
+      case 0:
+        return NewsTab();
+      case 1:
+        return VideoTab();
+      case 2:
+        return PhotoTab();
+      case 3:
+        return FavouriteTab();
+      default:
+        return NewsTab();
+    }
+  }
+
+  void _onDrawerMenuItemClicked(String title) {
+    print('Title : ' + title);
+    switch (title) {
+      case 'News':
+        this.setState(() => this.selectedTabIndex = 0);
+        break;
+      case 'Video':
+        this.setState(() => this.selectedTabIndex = 1);
+        break;
+      case 'Photo':
+        this.setState(() => this.selectedTabIndex = 2);
+        break;
+      case 'Favorite':
+        this.setState(() => this.selectedTabIndex = 3);
+        break;
+      case 'About':
+        // display about us dialog box
+        break;
+      default:
+        this.setState(() => this.selectedTabIndex = 0);
     }
   }
 }

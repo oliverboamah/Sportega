210cf7c919b13c3a1aa89daeeb54705d263cd317
Oliver Boamah <oliverboamah@yahoo.com>
2019-10-12 13:18:34 +0000

    Update APP_DESCRIPTION in env.dart

--- lib/env.dart
+++ lib/env.dart
@@ -1,6 +1,6 @@
 const APP_NAME = 'Sportega';
 const APP_VERSION = '1.0.';
 const APP_DESCRIPTION =
-    'A sports news app to discover current sports news, watch matchday hightlights videos and explore sports celebrity lifestyle photos';
+    'A sports news app to discover current sports news, watch matchday video highlights and explore sports celebrities lifestyle photos';
 const APP_LOGO = 'assets/images/logo.png';
 const APP_SLOGAN = 'sports in your pocket';
